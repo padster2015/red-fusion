4e88e4cef8b5dd639355e85036b9025c287b1b49
Patrick Henry <paddy@MAC21.local>
2016-01-04 19:40:45 -0500

    git stash recover

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,21 @@ class CreateUsersTable extends Migration
             $table->string('password', 60);
             $table->rememberToken();
             $table->timestamps();
+            $table->string('password', 60);
+             $table->rememberToken();
+             $table->timestamps();
+           $table->string('Facebook_api');
+                      $table->string('Twitter_api');
+           $table->string('Googleplus_api');
+           $table->string('linkedin_api');
+           $table->string('SnapChat_api');
+           $table->string('Pinterest_api');
+           $table->string('Email_api');
+           $table->string('Affiliate_api');
+           $table->string('PaidSearch_api');
+           $table->string('Youtube_api');
+           $table->string('Deals_api');
+           $table->string('GA_api');
         });
     }
 
