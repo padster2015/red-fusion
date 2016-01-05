bc83919ab7e55837de4d549a4e1c61c8c88b379b
Patrick Henry <paddy@MAC21.local>
2016-01-04 21:00:36 -0500

    lost

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -7,18 +7,18 @@
     <div class="col-sm-12">
     	<h3>{{ Auth::user()->name }} Settings</h3>  
       <ul>
-      <li><i class="fa fa-link"></i><strong>Affiliate :</strong> {{ Auth::user()->Affiliate }} </li>
-      <li><i class="fa fa-money"></i><strong>Deals :</strong> {{ Auth::user()->Deals }} </li>
-      <li><i class="fa fa-envelope-o"></i><strong>Email :</strong> {{ Auth::user()->Email }} </li>
-      <li><i class="fa fa-facebook-official"></i><strong>Facebook :</strong> {{ Auth::user()->Facebook }} </li>
-      <li><i class="fa fa-google"></i><strong>GA :</strong> {{ Auth::user()->GA }} </li>
-      <li><i class="fa fa-google-plus"></i><strong>Googleplus :</strong> {{ Auth::user()->Googleplus }} </li>
-      <li><i class="fa fa-linkedin"></i><strong>linkedin :</strong> {{ Auth::user()->linkedin }} </li>
-      <li><i class="fa fa-search"></i><strong>PaidSearch :</strong> {{ Auth::user()->PaidSearch }} </li>
-      <li><i class="fa fa-pinterest-p"></i><strong>Pinterest :</strong> {{ Auth::user()->Pinterest }} </li>
-      <li><i class="fa fa-camera"></i><strong>SnapChat :</strong> {{ Auth::user()->SnapChat }} </li>
-      <li><i class="fa fa-twitter"></i><strong>Twitter :</strong> {{ Auth::user()->Twitter }} </li>
-      <li><i class="fa fa-youtube"></i><strong>Youtube :</strong> {{ Auth::user()->Youtube }} </li>
+      <li><i class="fa fa-link"></i> &nbsp; <strong>Affiliate :</strong> {{ Auth::user()->Affiliate }} </li>
+      <li><i class="fa fa-money"></i> &nbsp; <strong>Deals :</strong> {{ Auth::user()->Deals }} </li>
+      <li><i class="fa fa-envelope-o"> &nbsp; </i><strong>Email :</strong> {{ Auth::user()->Email }} </li>
+      <li><i class="fa fa-facebook-official"></i> &nbsp; <strong>Facebook :</strong> {{ Auth::user()->Facebook }} </li>
+      <li><i class="fa fa-google"></i> &nbsp; <strong>GA :</strong> {{ Auth::user()->GA }} </li>
+      <li><i class="fa fa-google-plus"></i> &nbsp; <strong>Googleplus :</strong> {{ Auth::user()->Googleplus }} </li>
+      <li><i class="fa fa-linkedin"></i> &nbsp; <strong>linkedin :</strong> {{ Auth::user()->linkedin }} </li>
+      <li><i class="fa fa-search"></i> &nbsp; <strong>PaidSearch :</strong> {{ Auth::user()->PaidSearch }} </li>
+      <li><i class="fa fa-pinterest-p"></i> &nbsp; <strong>Pinterest :</strong> {{ Auth::user()->Pinterest }} </li>
+      <li><i class="fa fa-camera"></i> &nbsp; <strong>SnapChat :</strong> {{ Auth::user()->SnapChat }} </li>
+      <li><i class="fa fa-twitter"></i> &nbsp; <strong>Twitter :</strong> {{ Auth::user()->Twitter }} </li>
+      <li><i class="fa fa-youtube"></i> &nbsp; <strong>Youtube :</strong> {{ Auth::user()->Youtube }} </li>
 
       
 </ul>
