d66f9df566c4831c5cdb15f22d6aa8319be668eb
Patrick Henry <paddy@MAC21.local>
2016-01-04 21:07:09 -0500

    _api

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -8,17 +8,17 @@
     	<h3>{{ Auth::user()->name }} Settings</h3>  
       <ul>
       <li><i class="fa fa-link"></i> &nbsp; <strong>Affiliate :</strong> {{ Auth::user()->Affiliate_api }} </li>
-      <li><i class="fa fa-money"></i> &nbsp; <strong>Deals :</strong> {{ Auth::user()->Deals }} </li>
-      <li><i class="fa fa-envelope-o"> &nbsp; </i><strong>Email :</strong> {{ Auth::user()->Email }} </li>
-      <li><i class="fa fa-facebook-official"></i> &nbsp; <strong>Facebook :</strong> {{ Auth::user()->Facebook }} </li>
-      <li><i class="fa fa-google"></i> &nbsp; <strong>GA :</strong> {{ Auth::user()->GA }} </li>
-      <li><i class="fa fa-google-plus"></i> &nbsp; <strong>Googleplus :</strong> {{ Auth::user()->Googleplus }} </li>
-      <li><i class="fa fa-linkedin"></i> &nbsp; <strong>linkedin :</strong> {{ Auth::user()->linkedin }} </li>
-      <li><i class="fa fa-search"></i> &nbsp; <strong>PaidSearch :</strong> {{ Auth::user()->PaidSearch }} </li>
-      <li><i class="fa fa-pinterest-p"></i> &nbsp; <strong>Pinterest :</strong> {{ Auth::user()->Pinterest }} </li>
-      <li><i class="fa fa-camera"></i> &nbsp; <strong>SnapChat :</strong> {{ Auth::user()->SnapChat }} </li>
-      <li><i class="fa fa-twitter"></i> &nbsp; <strong>Twitter :</strong> {{ Auth::user()->Twitter }} </li>
-      <li><i class="fa fa-youtube"></i> &nbsp; <strong>Youtube :</strong> {{ Auth::user()->Youtube }} </li>
+      <li><i class="fa fa-money"></i> &nbsp; <strong>Deals :</strong> {{ Auth::user()->Deals_api }} </li>
+      <li><i class="fa fa-envelope-o"> &nbsp; </i><strong>Email :</strong> {{ Auth::user()->Email_api }} </li>
+      <li><i class="fa fa-facebook-official"></i> &nbsp; <strong>Facebook :</strong> {{ Auth::user()->Facebook_api }} </li>
+      <li><i class="fa fa-google"></i> &nbsp; <strong>GA :</strong> {{ Auth::user()->GA_api }} </li>
+      <li><i class="fa fa-google-plus"></i> &nbsp; <strong>Googleplus :</strong> {{ Auth::user()->Googleplus_api }} </li>
+      <li><i class="fa fa-linkedin"></i> &nbsp; <strong>linkedin :</strong> {{ Auth::user()->linkedin_api }} </li>
+      <li><i class="fa fa-search"></i> &nbsp; <strong>PaidSearch :</strong> {{ Auth::user()->PaidSearch_api }} </li>
+      <li><i class="fa fa-pinterest-p"></i> &nbsp; <strong>Pinterest :</strong> {{ Auth::user()->Pinterest_api }} </li>
+      <li><i class="fa fa-camera"></i> &nbsp; <strong>SnapChat :</strong> {{ Auth::user()->SnapChat_api }} </li>
+      <li><i class="fa fa-twitter"></i> &nbsp; <strong>Twitter :</strong> {{ Auth::user()->Twitter_api }} </li>
+      <li><i class="fa fa-youtube"></i> &nbsp; <strong>Youtube :</strong> {{ Auth::user()->Youtube_api }} </li>
 </ul>
     </div>
   </div><!--/row-->
