126d1258010e65579c869a0f65110eae3aaa1738
Patrick Henry <paddy@MAC21.local>
2016-01-04 19:15:07 -0500

    remove pie

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -55,7 +55,7 @@ $(function () {
             name: 'Joe',
             data: [4, 3, 3, 9, 0]
         }, {
-            type: 'area',
+            type: 'spline',
             name: 'Average',
             data: [3, 2.67, 3, 6.33, 3.33],
             marker: {
@@ -63,22 +63,7 @@ $(function () {
                 lineColor: Highcharts.getOptions().colors[3],
                 fillColor: 'white'
             }
-        }, {
-            type: 'pie',
-            name: 'Total consumption',
-            data: [{
-                name: 'Jane',
-                y: 13,
-                color: Highcharts.getOptions().colors[0] // Jane's color
-            }, {
-                name: 'John',
-                y: 23,
-                color: Highcharts.getOptions().colors[1] // John's color
-            }, {
-                name: 'Joe',
-                y: 19,
-                color: Highcharts.getOptions().colors[2] // Joe's color
-            }],
+        }
             center: [100, 80],
             size: 100,
             showInLegend: true,
