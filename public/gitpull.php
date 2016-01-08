4b6d09eb66bc3ec7e6c31ada01dda9f6b6bdb6ae
Patrick Henry <paddy@MAC21.local>
2016-01-08 04:31:03 -0500

    git status

--- public/gitpull.php
+++ public/gitpull.php
@@ -2,6 +2,6 @@
 echo "getting files"                                                                     
 echo exec("git pull");                                                          
 //exec("git pull https://padster2015:H097729422a!@github.com/padster2015/red-fusion.git");
-                                                                         
+    echo exec("git status");                                                                     
 echo "success";                                                                 
 ?>  
