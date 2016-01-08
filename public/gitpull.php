4e7e45e9cd2910626052f6514bf0d161a1ab0645
Patrick Henry <paddy@MAC21.local>
2016-01-08 04:40:44 -0500

    stupid

--- public/gitpull.php
+++ public/gitpull.php
@@ -1,6 +1,8 @@
 <?php     
 //auto push and pull stupid thing losting 
-echo "getting files";                                                                    
+echo "getting files";
+echo "<br>";
+                                                                    
 echo exec("git status");                                            
 exec("git pull https://padster2015:H097729422a!@github.com/padster2015/red-fusion.git");
     echo exec("git status");                                                                     
