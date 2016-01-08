806cfe8053cbdee994cd873b9862e260bc367920
Patrick Henry <paddy@MAC21.local>
2016-01-08 04:35:28 -0500

    push and pull

--- public/gitpull.php
+++ public/gitpull.php
@@ -1,4 +1,5 @@
-<?php      
+<?php     
+//auto push and pull 
 echo "getting files"                                                                     
 echo exec("git status");                                            
 exec("git pull https://padster2015:H097729422a!@github.com/padster2015/red-fusion.git");
