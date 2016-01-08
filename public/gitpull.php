6ca4a72bd7129f61f99645de36e4a96b6a8fdb2f
Patrick Henry <paddy@MAC21.local>
2016-01-08 04:38:47 -0500

    fix

--- public/gitpull.php
+++ public/gitpull.php
@@ -1,6 +1,6 @@
 <?php     
 //auto push and pull stupid thing losting 
-echo "getting files"                                                                     
+echo "getting files";                                                                    
 echo exec("git status");                                            
 exec("git pull https://padster2015:H097729422a!@github.com/padster2015/red-fusion.git");
     echo exec("git status");                                                                     
