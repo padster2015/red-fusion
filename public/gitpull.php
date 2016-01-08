dc859ec95758781642a03b343bbeedce49df4aec
Patrick Henry <paddy@MAC21.local>
2016-01-08 04:29:18 -0500

    clean up

--- public/gitpull.php
+++ public/gitpull.php
@@ -1,5 +1,9 @@
-
-<?php
-//lost
-
- `git pull`;?>
+<?php      
+echo "getting files"                                                                     
+echo exec("git pull");                                                          
+//exec("git pull https://padster2015:H097729422a!@github.com/padster2015/red-fu$
+?>                                                                              
+                                                                                
+<?php                                                                           
+echo "success";                                                                 
+?>  
