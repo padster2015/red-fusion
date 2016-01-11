9e13bada910507d37f2b6dd1a583e33241984f34
Patrick Henry <paddy@MAC21.local>
2016-01-11 03:08:42 -0500

    chart change

--- resources/views/budget/budget.blade.php
+++ resources/views/budget/budget.blade.php
@@ -20,6 +20,7 @@
 
 <h1> Current Budget</h1>
 <div id="Current" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
+<button id="button">Set new data</button>
 
 <script>
 $(function () {
@@ -77,6 +78,19 @@ $(function () {
     });
 });
 </script>
+
+<script>
+//update function based on type
+$(function () {
+    
+    // The button action
+    $('#button').click(function() {
+        chart.series[0].setData([129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 29.9, 71.5, 106.4] );
+    });
+});
+
+</script>
+
 
 <li><strong>facebook:$5000</li> 
 <li><strong>twitter:$5000</li> 
