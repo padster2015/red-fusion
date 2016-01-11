4b071a620a9c48d6aa3368e79cc104def62b444e
Patrick Henry <paddy@MAC21.local>
2016-01-11 02:47:25 -0500

    casks

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -22,15 +22,15 @@
               <h4>Clicks</h4>
             </div>
              <div class="col-xs-6 col-sm-3 placeholder text-center positive" style="border-radius:100%; background-color:#2ecc71; color:#FFF; width:200px; height:200px; padding-top:36px; margin-right:10%;">
-            <h1 class="timer"><strong>#5000</strong> </h1>
+            <h1 class="impressions"><strong>#5000</strong> </h1>
               <h4>Impressions</h4>
             </div>
              <div class="col-xs-6 col-sm-3 placeholder text-center negative " style="border-radius:100%; background-color:#c0392b; color:#FFF; width:200px; height:200px; padding-top:36px; margin-right:10%;">
-            <h1 class="timer"><strong>$10,000</strong> </h1>
+            <h1 class="spend"><strong>$10,000</strong> </h1>
               <h4>Spend to Date</h4>
             </div>
              <div class="col-xs-6 col-sm-3 placeholder text-center positive" style="border-radius:100%; background-color:#2ecc71; color:#FFF; width:200px; height:200px; padding-top:36px; margin-right:10%;">
-            <h1 class="timer"><strong>$20,000 </strong></h1>
+            <h1 class="roi"><strong>$20,000 </strong></h1>
               <h4>ROI</h4>
             </div>
           </div>
@@ -413,6 +413,44 @@ $('#change_chart_title').click(function(){
 
 jQuery(function($) {
         $('.clicks').countTo({
+            from: 0,
+            to: 10000,
+            speed: 1000,
+            refreshInterval: 50,
+            onComplete: function(value) {
+                console.debug(this);
+            }
+        });
+    });
+
+
+
+jQuery(function($) {
+        $('.impressions').countTo({
+            from: 0,
+            to: 10000,
+            speed: 1000,
+            refreshInterval: 50,
+            onComplete: function(value) {
+                console.debug(this);
+            }
+        });
+    });
+
+jQuery(function($) {
+        $('.spend').countTo({
+            from: 0,
+            to: 10000,
+            speed: 1000,
+            refreshInterval: 50,
+            onComplete: function(value) {
+                console.debug(this);
+            }
+        });
+    });
+
+jQuery(function($) {
+        $('.roi').countTo({
             from: 0,
             to: 10000,
             speed: 1000,
