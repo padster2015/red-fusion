cbb998b30889e4762e63b155d6cd08668ff4474f
Patrick Henry <paddy@MAC21.local>
2016-01-10 23:23:33 -0500

    padding

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -19,7 +19,7 @@
           {{ Auth::user()->Business_name }} Charts</h1>
 
           <div class="row placeholders">
-            <div class="col-xs-6 col-sm-3 placeholder text-center" style="border-radius:100%; background-color:#000; color:#FFF; width:200px; height:200px; padding-top:50%">
+            <div class="col-xs-6 col-sm-3 placeholder text-center" style="border-radius:100%; background-color:#000; color:#FFF; width:200px; height:200px; padding-top:10px;">
             <h1>#5000 </h1>
               <h4>clicks</h4>
             </div>
