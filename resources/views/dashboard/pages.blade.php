4e4ef3d50c5e48dd812eba22d1f464ffed1a762c
Patrick Henry <paddy@MAC21.local>
2016-01-10 23:10:20 -0500

    analytics

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -13,6 +13,62 @@
     	<h3>{{ Auth::user()->Business_name }} Charts</h3>  
     </div>
 
+<section class="rankingSection">
+        <div class="rankingItems">
+            <div class="rankingItem fadeInDown rankingItem--global" data-view="WebsitePageModule.Views.RankingView" data-rank-subject="Global">
+                
+
+<div class="rankingItem-content">
+    <span class="rankingItem-title"><i class="rankingItem-icon icon-geo"></i>Global Rank<i class="tooltip informationButton owMrkClick" original-title="Traffic rank of site, as compared to all other sites in the world"></i></span>
+    <div itemprop="url" class="rankingItem-underTitle">
+<a class="rankingItem-subTitle is-link" data-analytics-category="Internal Link" data-analytics-label="Global Rank/Worldwide" href="/global" itemprop="significantLink">Worldwide</a>            </div>
+    <div class="rankingItem-rank js-editable">
+                <span class="rankingItem-value js-countable" data-value="31,567">#31,567</span><i class="rankingItem-valueIcon rankingItem-valueIcon--increased icon-arrow-down-ranking rankingItem-valueIcon--is-shown"></i>
+                    
+
+    </div>
+</div>
+<div class="rankingItem-embed"></div>
+            </div>
+            <div class="rankingItem fadeInDown rankingItem--country" data-view="WebsitePageModule.Views.RankingView" data-rank-subject="Country">
+
+
+                
+
+<div class="rankingItem-content">
+    <span class="rankingItem-title"><i class="rankingItem-icon icon-country-rank"></i>Country Rank<i class="tooltip informationButton owMrkClick" original-title="Traffic rank of site, as compared to all other sites in its leading country"></i></span>
+    <div itemprop="url" class="rankingItem-underTitle">
+<a class="rankingItem-subTitle is-link" href="/country/united_states" itemprop="significantLink" data-analytics-category="Internal Link" data-analytics-label="Country Rank/840">United States</a>            <i class="flag us"></i>            </div>
+    <div class="rankingItem-rank js-editable">
+                <span class="rankingItem-value js-countable" data-value="23,936">#23,936</span><i class="rankingItem-valueIcon rankingItem-valueIcon--increased icon-arrow-down-ranking rankingItem-valueIcon--is-shown"></i>
+                    
+
+    </div>
+</div>
+<div class="rankingItem-embed"></div>
+            </div>
+            <div class="rankingItem fadeInDown rankingItem--category" data-view="WebsitePageModule.Views.RankingView" data-rank-subject="Category">
+
+
+                
+
+<div class="rankingItem-content">
+    <span class="rankingItem-title"><i class="rankingItem-icon categories-sprite small computer_and_electronics"></i>Category Rank<i class="tooltip informationButton owMrkClick" original-title="Traffic rank of site, as compared to all other sites in its main category"></i></span>
+    <div itemprop="url" class="rankingItem-underTitle">
+<a class="rankingItem-subTitle is-link" href="/category/computer_and_electronics" itemprop="significantLink" data-analytics-category="Internal Link" data-analytics-label="Category Rank/computer_and_electronics">Computer and Electronics</a>                    <a class="rankingItem-categoryEdit js-editCategory" data-options="{&quot;domain&quot;:&quot;Bootply.com&quot;,&quot;category&quot;: &quot;Computer_and_Electronics&quot;,&quot;categoryClass&quot;:&quot;&quot;,&quot;categoryLabel&quot;: &quot;Computer and Electronics&quot;}" itemprop="name">Edit</a>
+    </div>
+    <div class="rankingItem-rank js-editable">
+                <span class="rankingItem-value js-countable" data-value="1,729">#1,729</span><i class="rankingItem-valueIcon rankingItem-valueIcon--increased icon-arrow-down-ranking rankingItem-valueIcon--is-shown"></i>
+                    
+
+    </div>
+</div>
+<div class="rankingItem-embed"></div>
+            </div>
+        </div>
+    </section>
+
+
 
   </div><!--/row-->
   <hr>
