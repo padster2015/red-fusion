e3bf9333184fd1885bfd985201296a421633bebb
Patrick Henry <paddy@MAC21.local>
2016-01-10 21:03:15 -0500

    git push

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -7,11 +7,12 @@
     <div class="col-sm-12">
 <form action="deliver()">
 <SELECT NAME="Network" MULTIPLE SIZE=5> 
-<OPTION VALUE="Facebook">Facebook 
-<OPTION VALUE="LinkedIn">LinkedIn
- <OPTION VALUE="Twitter">Twitter 
- <OPTION VALUE="Pinterst">Pinterst 
- <OPTION VALUE="olives">SnapChat</SELECT>
+<OPTION VALUE="Facebook">Facebook></OPTION>
+<OPTION VALUE="LinkedIn">LinkedIn></OPTION>
+ <OPTION VALUE="Twitter">Twitter></OPTION>
+ <OPTION VALUE="Pinterst">Pinterst></OPTION> 
+ <OPTION VALUE="olives">SnapChat></OPTION>
+ </SELECT>
 
 <textarea editable="true" placeholder="Craft Post" id="Craft_Post"></textarea>
 <button>Send</button>
