6b8dfb705cafdaf771dcde9425aaa3c31c40fee7
Patrick Henry <paddy@bknstg01.intra.rakuten.co.jp>
2016-01-12 01:07:52 -0500

    fix

--- resources/views/budget/budget.blade.php
+++ resources/views/budget/budget.blade.php
@@ -45,7 +45,7 @@ backgroundColor: null
             text: 'Budget by channel',
 style: {
          color: '#FFF',
-         font-weight:'200',
+         fontweight:'200',
          font: '25px "Work Sans", Helvetica, sans-serif'
       }
         },
