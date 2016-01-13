769318b1e81504420e58ab6e771022cb4b242c56
Patrick Henry <git@patrickhenry.co.uk>
2016-01-13 09:41:06 -0500

    Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,13 @@
                 <div class="panel-body">
                    <h1>Red Fusion</h1>
                    <br>
-                   <p><strong>Red Fusion</strong> is your Digital Marketing Solution from budgeting, posting and reporitng
-                   </p>
+                   <p><strong>Red Fusion</strong> is your Digital Marketing Solution from budgeting, social media posting to full market analytics. </p>
+<br>
+<p>
+if your a small business or local sports or a international organisation <p> are full digital and omni marketing approach tool will allow you to see whats happening and make the decissions you need to grow your online precsence. In the most cost effective way. </p>
+
+<a href="/register"> register now</a>
+
                 </div>
             </div>
         </div>
