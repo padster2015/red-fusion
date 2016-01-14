76f648739104bc940801e261ec92e5571d793624
Patrick Henry <git@patrickhenry.co.uk>
2016-01-14 13:17:26 -0500

    background null

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -333,7 +333,7 @@ $(document).ready(function() { /*begin chart render*/
             borderWidth: 0,
             pointWidth: 10,
             shadow: false,
-            backgroundColor: '#e2dfd3'
+            backgroundColor: null
         },
         title: {
             text: 'Pie Test'
