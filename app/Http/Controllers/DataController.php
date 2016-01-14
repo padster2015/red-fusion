08a98257aac8f2dbadcca441474d550261216642
Patrick Henry <paddy@MAC21.local>
2016-01-13 21:20:01 -0500

    store

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -39,7 +39,7 @@ class DataController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        echo "store";
     }
 
     /**
@@ -50,7 +50,7 @@ class DataController extends Controller
      */
     public function show($id)
     {
-        //
+        echo "show message id";
     }
 
     /**
