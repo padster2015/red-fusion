42e2ec736dcf7209827833ad4b81a478a5fed971
Patrick Henry <paddy@MAC21.local>
2016-01-19 04:33:51 -0500

    kasper

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -32,7 +32,7 @@ class DataController extends Controller
     {
         $DBClicks = DB::table('TotalClicks')->select('Total_Clicks')->get();
 //return View::make('dashboard.pages')->with('data', $DBClicks);
-    return view('dashboard.pages')->with($DBClicks);
+    return view('dashboard.pages')->with(['total_clicks' => $DBClicks])`;
     }
 
 
