072916502b7209e9cd9c432ea1d428fc8c6ccef4
Patrick Henry <paddy@MAC21.local>
2016-01-19 01:20:52 -0500

    controller click

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -30,7 +30,8 @@ class DataController extends Controller
 
     public function Dashboard_Clicks()
     {
-        $DBClicks = DB::table('Aff Clicks')->sum('no_of_Clicks');
+        $DBClicks = DB::table('Aff Clicks')->sum('no_of_Clicks')->get();
+        
         echo json_encode($DBClicks,JSON_NUMERIC_CHECK);
     }
     /**
