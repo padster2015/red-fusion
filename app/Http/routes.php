deb9e644b6b0322da7c3e84c263cfa9172ba08c8
Patrick Henry <git@patrickhenry.co.uk>
2016-01-19 11:14:26 -0500

    API endpoints

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,6 +35,27 @@ Route::get('/api/v1/Data/channel/{channel}/messageID/{message-id}/metric/{metric
 
 Route::get('/api/v1/Data/save/', 'DataController@store');
 
+//API - routes
+
+Route::get('api', function() {
+    $routeCollection = Route::getRoutes();
+
+    echo "<table style='width:100%'>";
+    echo "<tr>";
+        echo "<td width='10%'><h4>HTTP Method</h4></td>";
+        echo "<td width='10%'><h4>Route</h4></td>";
+        echo "<td width='80%'><h4>Corresponding Action</h4></td>";
+    echo "</tr>";
+    foreach ($routeCollection as $value) {
+        echo "<tr>";
+            echo "<td>" . $value->getMethods()[0] . "</td>";
+            echo "<td>" . $value->getPath() . "</td>";
+            echo "<td>" . $value->getActionName() . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+});
+
 
 // Messaging
 
