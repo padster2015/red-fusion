2b8d6233c686abe6db4bfb3243db8fc201100bf3
Patrick Henry <paddy@MAC21.local>
2016-01-19 04:44:38 -0500

    sdfg

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -31,7 +31,8 @@ class DataController extends Controller
     public function Dashboard_Clicks()
     {
         $DBClicks = DB::table('TotalClicks')->select('Total_Clicks')->get();
-    return view('dashboard.pages')->with($DBClicks);
+    $data = json_encode($DBClicks,JSON_NUMERIC_CHECK);
+    return view('dashboard.pages')->with($data);
     }
 
 
