6665117c07f28810fc96faf547033ed4452179d0
Patrick Henry <paddy@MAC21.local>
2016-01-19 00:44:43 -0500

    app provider

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        'Barryvdh\Debugbar\ServiceProvider', . 
         /*
          * Application Service Providers...
          */
