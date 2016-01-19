6990c32c2e6493b240aa3fd5aacb4c1cc601dc91
Patrick Henry <paddy@MAC21.local>
2016-01-18 20:08:52 -0500

    Click Dashboard Data

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -18,7 +18,7 @@
 
           <div class="row placeholders chart-highlights" style="text-align=Center;">
             <div class="col-xs-6 col-sm-3 placeholder text-center circlefactsp" style="border-radius:100%; background-color:#2ecc71; color:#FFF; width:200px; height:200px; padding-top:36px; margin-left:2%; margin-right:7%;">
-            <h1 class="clicks"><strong>#5000</strong> </h1>
+            <h1 class="clicks"><strong>{{ DataController::Dashboard_Clicks()->no_of_Clicks }} </strong> </h1>
               <h4>Clicks</h4>
             </div>
              <div class="col-xs-6 col-sm-3 placeholder text-center positive  " style="border-radius:100%; background-color:#2ecc71; color:#FFF; width:200px; height:200px; padding-top:36px; margin-right:7%;">
