04922f5692198c599f5a940596dd8bc64115b93a
Patrick Henry <paddy@MAC21.local>
2016-01-19 02:26:58 -0500

    gf

--- resources/views/dashboard/pages.blade.php
+++ resources/views/dashboard/pages.blade.php
@@ -14,7 +14,7 @@
         <div class="main-view" style="width:80%; margin:0 auto;" ng-app="myApp" ng-controller="customersCtrl">
           <ul>
   <li ng-repeat="x in myData">
-    @{{ x.First_Name x.Last_Name}}
+    @{{ x.First_Name}}
   </li>
 </ul>
           <h1>
