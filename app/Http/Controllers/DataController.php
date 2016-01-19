d43d2d32870f6965303ff60481b84a954ca89016
Patrick Henry <paddy@MAC21.local>
2016-01-19 04:15:43 -0500

    sdfg

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -32,7 +32,7 @@ class DataController extends Controller
     {
         $DBClicks = DB::table('TotalClicks')->select('Total_Clicks')->get();
 //return View::make('dashboard.pages')->with('data', $DBClicks);
-    return view('dashboard.pages')->with($DBClicks->'Total_Clicks');
+    return view('dashboard.pages')->with($DBClicks->{Total_Clicks});
     }
     /**
      * Show the form for creating a new resource.
