e7fe7066cde5c399c437e75847824bf6bb8958a0
Patrick Henry <git@patrickhenry.co.uk>
2016-03-22 12:55:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@ composer install
 chmod -R gu+w storage && chmod -R guo+w storage && chmod 777 -R * && chown apache:apache -R *
 cp -a httpd.conf /etc/httpd/conf/httpd.conf
 mkdir /root/.config
-mkdir /root/.config/.git
+mkdir /root/.config/git
+mkdir /root/.config/git/attributes
+mkdir /root/.config/git/ignore
 chmod 777 -R /root/.config
