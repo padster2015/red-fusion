145c1b15dc8aacd3f48c831a00b398118f31836e
Patrick <patrick.henry@linkshare.com>
2016-03-31 01:57:57 -0400

    welcome page update
    welcome page update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,14 +10,14 @@
                 <div class="panel-body">
                    <h1>Red Fusion v1</h1>
                    <br>
-                   <p><strong>Red Fusion</strong> is your Digital Marketing Solution from budgeting, social media posting to full market analytics. </p>
+                   <p><strong>Red Fusion</strong>  is your Digital Marketing Solution from budgeting, social media and posting to full market analytics. View your business functionality on a clean effective dashboard.</p>
 <br>
 <p>
-if your a small business or local sports or a international organisation <p> are full digital and omni marketing approach tool will allow you to see whats happening and make the decissions you need to grow your online precsence. In the most cost effective way. </p>
+If you are a small business, local sports team or an international organisation this is for you.
+This solution is a full digital and omni marketing approach tool that  will allow you to see what is happening. It can make the decisions you need to grow your online presence in the most cost effective way. </p>
 
-<h2> if you are from peek.usertesting.com please click login and use the provided info to login :) </h2>
-this app is in development and not all functiionality is finished.
-
+<h2> If you are from peek.usertesting.com please click login and use the provided info to login :) </h2>
+this app is in development and not all functionality is finished.
 <a href="/register"> register now!!</a>
 
                 </div>
