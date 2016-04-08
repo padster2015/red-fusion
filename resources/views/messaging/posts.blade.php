24078ba8c275b60bf0547b644e34b92a3e491811
Patrick <patrick.henry@linkshare.com>
2016-04-08 17:38:23 -0400

    superman

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -133,6 +133,7 @@ window.setTimeout(function() { $(".alert-message").alert('close'); }, 2000);
                        success : function (d)
                        {
                        alert(CraftPostIn);
+                       $('#messages-grid').bootstrapTable('refresh');
                        },
                        })
                 
