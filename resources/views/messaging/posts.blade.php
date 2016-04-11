1d7c612b8fde2808dac4cab7b46eb4ae251b9986
Patrick <patrick.henry@linkshare.com>
2016-04-10 23:38:53 -0400

    dropdown

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -32,19 +32,17 @@
 
 <div class="dropdown">
   <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-    Choose Hashtag
+    Dropdown
     <span class="caret"></span>
   </button>
-  <ul id="HashTags" class="dropdown-menu" aria-labelledby="dropdownMenu1">
-    <li><a href="#">NY Year</a></li>
-    <li><a href="#">Happy Easter</a></li>
-    <li><a href="#">Summer Sale</a></li>
-    <li><a href="#">Black Friday</a></li>
-    <li><a href="#">Cyber Monday</a></li>
-    <li><a href="#">Merry Xmas</a></li>
+  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">Action</a></li>
+    <li><a href="#">Another action</a></li>
+    <li><a href="#">Something else here</a></li>
+    <li role="separator" class="divider"></li>
+    <li><a href="#">Separated link</a></li>
   </ul>
 </div>
-
 
 
 
