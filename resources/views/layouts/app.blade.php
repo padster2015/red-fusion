afb0384d6221e12b241df958984ff2e2bb62e980
Patrick Henry <git@patrickhenry.co.uk>
2016-04-11 11:01:47 -0400

    kllst

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,15 +19,6 @@
 
   <link href="app/css/style.css" rel="stylesheet">
 
-<link rel="stylesheet" href="app/css/bootstrap-table.css">
-<!--<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.0/animate.min.css" type="text/css" rel="stylesheet">-->
-
-<!-- Latest compiled and minified JavaScript -->
-
-
-
-
-
 <!-- JavaScripts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/velocity/1.2.2/velocity.min.js"></script>
@@ -40,34 +31,12 @@
 
 <script src="app/js/count-to.js"></script>
  <link href="app/css/style.css" rel="stylesheet">
-<script src="app/js/bootstrap-table.js"></script>
 
 
 
     <script src="https://code.highcharts.com/highcharts.js"></script>
 <script src="https://code.highcharts.com/modules/exporting.js"></script>
 <script src="http://code.highcharts.com/modules/drilldown.js"></script>
-<script src="app/js/extensions/sticky-header/bootstrap-table-sticky-header.js"></script>
-
-
-
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
-
-
-
-
-
-
-
-
-
-
-
-
 
 </head>
 <body id="app-layout">
