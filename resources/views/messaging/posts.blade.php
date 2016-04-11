ec11309483952704dbceee00e176a181a32e1424
Patrick <patrick.henry@linkshare.com>
2016-04-10 22:36:56 -0400

    hashtag

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -30,7 +30,45 @@
  <br><strong>#</strong>
 
 
+
 <i class="fa fa-hashtag"></i>
+
+
+
+<div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+    Choose Hashtag
+    <span class="caret"></span>
+  </button>
+  <ul id="Network" class="dropdown-menu" aria-labelledby="dropdownMenu1">
+    <li><a href="#">NY Year</a></li>
+    <li><a href="#">Happy Easter</a></li>
+    <li><a href="#">Summer Sale</a></li>
+    <li><a href="#">Black Friday</a></li>
+    <li><a href="#">Cyber Monday</a></li>
+    <li><a href="#">Merry Xmas</a></li>
+  </ul>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <Select id="HashTags" class="form-control">
 <option value="Sale">Sale
 <option value="Summer">Summer
