924d760db6303404a897a773a1b4cd29553100db
Patrick Henry <git@patrickhenry.co.uk>
2016-04-11 11:01:36 -0400

    wlecome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container spark-screen">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
+            <div class="panel panel-default" id="welcome">
                 <div class="panel-heading"><strong>Welcome</strong></div>
 
                 <div class="panel-body">
