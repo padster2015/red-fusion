83022fab6ec96321c616cb55098f4826ebdb087e
Patrick Henry <git@patrickhenry.co.uk>
2016-04-11 13:15:20 -0400

    oops

--- resources/views/budget/budget.blade.php
+++ resources/views/budget/budget.blade.php
@@ -26,7 +26,9 @@
 
 <h1> Current Budget</h1>
 <div id="Current" style="min-width: 310px; height: 400px; max-width: 600px; margin: 0 auto"></div>
-<form action="SetBudget()"> <button id="button" style="color:#000;" class="btn btn-default" onclick="SetBudget()">Set new budgets</button>
+
+<form action="SetBudget()"> 
+<button id="button" style="color:#000;" class="btn btn-default" onclick="SetBudget()">Set new budgets</button>
 </form>
 
 <script>
