d31a60254374b01c47a3ea23b1cc04153d536b86
Patrick Henry <git@patrickhenry.co.uk>
2016-04-11 12:10:42 -0400

    missing oclour

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,9 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-8 col-md-offset-2" id="welcome">
             <div class="panel panel-default">
-                <div class="panel-heading" id="welcome">Login</div>
+                <div class="panel-heading" >Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {!! csrf_field() !!}
