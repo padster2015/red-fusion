244417579a39adae14b1c9c55b6626601ca072c8
Patrick <patrick.henry@linkshare.com>
2016-04-13 01:28:49 -0400

    e
    e

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -2,10 +2,9 @@
 @section('content')
 {{
 
-$config['callback_url'] = 'http://web-1.redfusionstackwebndb.c3c1a3ca.cont.dockerapp.io/?fbTrue=true'; 
-
+$config['callback_url'] = 'http://web-1.redfusionstackwebndb.c3c1a3ca.cont.dockerapp.io/?fbTrue=true',
 //Facebook configuration
-$config['App_ID'] = '222866618076812';
+$config['App_ID'] = '222866618076812',
 $config['App_Secret']  ='f42af1b8213f429457e50cbfc16ce118'; 
 
 
