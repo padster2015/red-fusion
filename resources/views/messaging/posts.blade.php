a55fa5c65b11745d170bacf84de6a51cb674309d
Patrick <patrick.henry@linkshare.com>
2016-04-13 01:29:16 -0400

    ww

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -5,7 +5,7 @@
 $config['callback_url'] = 'http://web-1.redfusionstackwebndb.c3c1a3ca.cont.dockerapp.io/?fbTrue=true',
 //Facebook configuration
 $config['App_ID'] = '222866618076812',
-$config['App_Secret']  ='f42af1b8213f429457e50cbfc16ce118'; 
+$config['App_Secret']  ='f42af1b8213f429457e50cbfc16ce118',
 
 
 
