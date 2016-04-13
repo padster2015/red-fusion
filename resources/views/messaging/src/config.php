ddd74b70406c9f2a5bfa164823a12e06e3d3b6ca
Patrick <patrick.henry@linkshare.com>
2016-04-13 01:14:54 -0400

    config update

--- resources/views/messaging/src/config.php
+++ resources/views/messaging/src/config.php
@@ -1,9 +1,8 @@
-<?php
-
-$config['callback_url']         =   'http://web-1.redfusionstackwebndb.c3c1a3ca.cont.dockerapp.io/?fbTrue=true'; //   /?fbTrue=true allow you to code process section.
+{{
+$config['callback_url'] = 'http://web-1.redfusionstackwebndb.c3c1a3ca.cont.dockerapp.io/?fbTrue=true'; 
 
 //Facebook configuration
-$config['App_ID']      =   '222866618076812';
+$config['App_ID'] = '222866618076812';
 $config['App_Secret']  =   'f42af1b8213f429457e50cbfc16ce118'; 
 
-?>
+}}
