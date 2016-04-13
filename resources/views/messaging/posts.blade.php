9d7809add6d5505fb4bc52ef0285f204ec1e83c8
Patrick <patrick.henry@linkshare.com>
2016-04-13 01:27:11 -0400

    css

--- resources/views/messaging/posts.blade.php
+++ resources/views/messaging/posts.blade.php
@@ -2,7 +2,14 @@
 @section('content')
 {{
 
-include './src/config.php',
+$config['callback_url'] = 'http://web-1.redfusionstackwebndb.c3c1a3ca.cont.dockerapp.io/?fbTrue=true'; 
+
+//Facebook configuration
+$config['App_ID'] = '222866618076812';
+$config['App_Secret']  ='f42af1b8213f429457e50cbfc16ce118'; 
+
+
+
 include './src/facebook.php'
 
 @if(isset($_POST['status']))
