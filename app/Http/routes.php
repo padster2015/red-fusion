8082f689f1e661feca65862f387fab6dbb40c768
Patrick <patrick.henry@linkshare.com>
2016-04-14 02:41:42 -0400

    socials

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -123,14 +123,6 @@ Route::get('/api/v1/Budget/DashboardSummary_SPTD/', 'DataController@SPTD');
 |
 */
 
-Route::get('github/authorize', function() {
-    return SocialAuth::authorize('github');
-});
-
-Route::get('github/login', function(){
-    SocialAuth::login('github');
-    return 'done';
-});
 
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
@@ -141,6 +133,42 @@ Route::group(['middleware' => 'web'], function () {
     Route::get('/', function () {
         return view('welcome');
     });
+
+
+
+    Route::get('auth/authorize/{provider}',  function($provider) {
+        return SocialAuth::authorize($provider);
+    });
+    Route::get('auth/login/{provider}',  function($provider) {
+        try {
+            SocialAuth::login($provider, function($user, $details) {
+                $user->username = $details->nickname;
+                $user->name = $details->full_name;
+                $user->save();
+            });
+        } catch (ApplicationRejectedException $e) {
+            // User rejected application
+            dd('error1');
+        } catch (InvalidAuthorizationCodeException $e) {
+            // Authorization was attempted with invalid
+            // code,likely forgery attempt
+            print_r($provider);
+            dd($e);
+        }
+
+        // Current user is now available via Auth facade
+        $user = Auth::user();
+        dd($user);
+
+        return $user;
+    });
+
+
+
+
+
+
+
 
 
     // dashboard from after login!
