65699874457a32f49e8df2559e60c86e5e3fe584
Patrick <patrick.henry@linkshare.com>
2016-04-19 13:28:47 -0400

    remove temp work

--- public/1/src/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$config['callback_url']         =   'http://web-1.redfusionstackwebndb.b97ed22a.cont.dockerapp.io/1/index.php?fbTrue=true'; //   /?fbTrue=true allow you to code process section.
-
-//Facebook configuration
-$config['App_ID']      =   '222866618076812';
-$config['App_Secret']  =   'f42af1b8213f429457e50cbfc16ce118'; 
-
-?>
--- public/3/delete_post.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	require 'src/config.php';
-	
-	$del = "delete from post_details where id=".$_GET['pId']." AND uid=".$_SESSION['usrId'];
-	mysql_query($del);
-	
-	header("location:index.php");
-?>
